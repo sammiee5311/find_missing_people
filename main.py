4ef388935f7ea3e64e00d43e0617a7473d229786
Sam.An <56215891+sammiee5311@users.noreply.github.com>
2020-08-27 22:30:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from mail import Inform
 import video_face_recog
 
 
-alert = Inform()
+inform = Inform()
 video_face_recog = video_face_recog.face_recog('./Images','',names=[], wanted_names=[])
 video_face_recog.start_face_recog()
 wanted_names = video_face_recog.get_names()
 cv2.destroyAllWindows()
 print(wanted_names)
 for name in wanted_names:
-    alert.send(name + '.jpg', name, wanted_names[name])
+    inform.send(name + '.jpg', name, wanted_names[name])

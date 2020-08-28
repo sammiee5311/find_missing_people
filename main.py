3afa1f31c4ad611ac9cd993922cd739e74a06130
Sam.An <56215891+sammiee5311@users.noreply.github.com>
2020-08-28 14:48:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,8 @@ inform = Inform()
 
 ##### GET THE NAMES AND PICTURE FROM SERVER #####
 
-while True:
-    video_face_recog = video_face_recog.face_recog('./Images', wanted_names=['Sammy'])
-    video_face_recog.start_face_recog()
-    wanted_names = video_face_recog.get_names()
-    for name in wanted_names:
-    inform.send(name + '.jpg', name, wanted_names[name])
+video_face_recog = video_face_recog.face_recog('./Images', wanted_names=['Sammy'])
+video_face_recog.start_face_recog()
+wanted_names = video_face_recog.get_names()
+for name in wanted_names:
+inform.send(name + '.jpg', name, wanted_names[name])

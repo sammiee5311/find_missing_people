fe191936cf3ef60b44dc33e7db44c02560e3f1f9
Sam.An <56215891+sammiee5311@users.noreply.github.com>
2020-08-28 14:41:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import cv2
 from mail import Inform
 import video_face_recog
 
-
 inform = Inform()
-video_face_recog = video_face_recog.face_recog('./Images','',names=[], wanted_names=[])
-video_face_recog.start_face_recog()
-wanted_names = video_face_recog.get_names()
-cv2.destroyAllWindows()
-print(wanted_names)
-for name in wanted_names:
+
+##### GET THE NAMES AND PICTURE FROM SERVER #####
+
+while True:
+    video_face_recog = video_face_recog.face_recog('./Images', wanted_names=['Sammy'])
+    video_face_recog.start_face_recog()
+    wanted_names = video_face_recog.get_names()
+    for name in wanted_names:
     inform.send(name + '.jpg', name, wanted_names[name])

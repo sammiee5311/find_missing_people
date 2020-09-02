0f94dc8717d07e610b1f34253730d0c81b85ff54
Sam.An <56215891+sammiee5311@users.noreply.github.com>
2020-09-02 23:31:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import cv2
 from mail import Inform
 import video_face_recog
+import server
 
 inform = Inform()
 
-##### GET THE NAMES AND PICTURE FROM SERVER #####
+serve = server.From_server() # write url
+serve.read_csv()
+wanted_names = serve.download_images() # write path where you want to download images
 
-video_face_recog = video_face_recog.face_recog('./Images path', wanted_names=['name'])
+video_face_recog = video_face_recog.face_recog('./Images path', wanted_names=wanted_names)
 video_face_recog.start_face_recog()
 wanted_names = video_face_recog.get_names()
 for name in wanted_names:
